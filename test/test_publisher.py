32af7df7319fc5abaf4e6bf020be603aec384857
Enrico Minack <github@enrico.minack.dev>
2020-11-11 21:57:11 +0100

    Make a test fail

--- test/test_publisher.py
+++ test/test_publisher.py
@@ -843,3 +843,6 @@ class TestPublisher(unittest.TestCase):
         publisher.hide_comment.assert_has_calls(
             [mock.call('comment one'), mock.call('comment two')], any_order=False
         )
+
+    def test_fail(self):
+        self.fail('just testing with failing test')
