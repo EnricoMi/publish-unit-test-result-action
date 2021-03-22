5acd82ffb7f4bc792b26cd350c26c60d11575e83
Enrico Minack <github@enrico.minack.dev>
2021-03-22 17:04:53 +0100

    Query for both build and docker action in badge

--- runkit.js
+++ runkit.js
@@ -1,12 +1,17 @@
-exports.endpoint = async function(request, response) {
-  var got = require("got");
-  url = 'https://api.github.com/search/code?q=%22publish-unit-test-result-action%22+path%3A.github%2Fworkflows%2F+language%3AYAML&type=Code'
+const opts = { headers: { Authorization: `bearer ${process.env['github.token']}` }, responseType: 'json' };
+var search = query => require("got")(`https://api.github.com/search/code?${query}`, opts);
 
-  var count = (await got(url, { headers: { Authorization: `bearer ${process.env['github.token']}` }, responseType: 'json' })).body.total_count;
- 
+exports.endpoint = async function(request, response) {
+  var query = q => `q=%22${q}%22+path%3A.github%2Fworkflows%2F+language%3AYAML&type=Code`;
+  var build_count = search(query('publish-unit-test-result-action'));
+  var docker_count = search(query('enricomi%2Fpublish-unit-test-result-action'));
+  var count = Promise.all([build_count, docker_count])
+                     .then((counts) => counts.map(c => c.body.total_count))
+                     .then((counts) => counts.reduce((a, b) => a + b, 0));
+
   var resp = {
     subject: 'GitHub Workflows',
-    status: count,
+    status: await count,
     color: "blue"
   }
 
