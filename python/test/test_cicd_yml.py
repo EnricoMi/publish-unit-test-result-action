75a3f2ccc027e9f3d26b1dff70958a00bc184f90
Enrico Minack <github@enrico.minack.dev>
2021-05-07 21:31:12 +0200

    Add test to keep docker image publisher test in sync with action.yaml

---
+++ python/test/test_cicd_yml.py
@@ -0,0 +1,29 @@
+import unittest
+
+import yaml
+from yaml import Loader
+
+
+class TestActionYml(unittest.TestCase):
+
+    def test_cicd_workflow(self):
+        with open('../../action.yml') as r:
+            action = yaml.load(r, Loader=Loader)
+
+        with open('../../.github/workflows/ci-cd.yml') as r:
+            cicd = yaml.load(r, Loader=Loader)
+
+        docker_image_steps = cicd.get('jobs', []).get('publish-docker-image', {}).get('steps', [])
+        docker_image_step = [step
+                             for step in docker_image_steps
+                             if step.get('name') == 'Publish Unit Test Results']
+        self.assertEqual(1, len(docker_image_step))
+        docker_image_run = docker_image_step[0].get('run')
+        self.assertTrue(docker_image_run)
+        vars = [var[6:].lower()
+                for line in docker_image_run.split('\n')
+                for part in line.split(' ')
+                for var in [part.strip()]
+                if var.startswith('INPUT_')]
+
+        self.assertEqual(sorted(action.get('inputs', {}).keys()), sorted(vars))
