53e8c5e6f6a52d2c9b795f2f0f8908beda83fe6f
Enrico Minack <github@enrico.minack.dev>
2022-03-02 17:57:31 +0100

    Ensure encoding of sys.stdout

--- python/publish/github_action.py
+++ python/publish/github_action.py
@@ -8,7 +8,12 @@ from publish import logger
 
 class GithubAction:
 
-    def __init__(self, file: TextIOWrapper = sys.stdout):
+    def __init__(self, file: TextIOWrapper = None):
+        if file is None:
+            file = sys.stdout
+            if isinstance(file, TextIOWrapper):
+                # ensure we have utf8 encoding, the default encoding of sys.stdout on Windows is cp1252
+                file.reconfigure(encoding='utf-8')
         self._file = file
 
     def set_output(self, name: str, value: Any) -> str:
