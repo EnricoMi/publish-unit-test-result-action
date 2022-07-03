1f7ad6558716ec980f7fcdfff5cd7ef447ec69a0
Enrico Minack <github@enrico.minack.dev>
2022-07-03 23:19:42 +0100

    Update CI docker image args (#319)

--- python/test/test_cicd_yml.py
+++ python/test/test_cicd_yml.py
@@ -5,6 +5,7 @@ import yaml
 
 project_root = pathlib.Path(__file__).parent.parent.parent
 
+
 class TestActionYml(unittest.TestCase):
 
     def test_cicd_workflow(self):
@@ -27,4 +28,4 @@ class TestActionYml(unittest.TestCase):
                 for var in [part.strip()]
                 if var.startswith('INPUT_')]
 
-        self.assertEqual(sorted(action.get('inputs', {}).keys()), sorted(vars))
+        self.assertEqual(sorted(list(action.get('inputs', {}).keys()) + ['log_level']), sorted(vars))
