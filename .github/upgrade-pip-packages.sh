7fca51902f43b9d5a6ebe3b8e6520cf8fbe66b2f
Enrico Minack <github@enrico.minack.dev>
2023-03-14 10:39:49 +0100

    Fix versions for transient dependencies, test in CI

---
+++ .github/upgrade-pip-packages.sh
@@ -0,0 +1,10 @@
+base="$(dirname "$0")"
+
+pip install --upgrade -r "$base/../python/requirements-direct.txt"
+
+pip install pipdeptree
+grep -e "^#" -e ";" "$base/../python/requirements-direct.txt" > "$base/../python/requirements.txt"
+pipdeptree --packages=$(grep -v "^#" "$base/../python/requirements-direct.txt" | sed -e "s/;.*//" -e "s/=.*//g" | paste -s -d ,) --freeze >> "$base/../python/requirements.txt"
+
+git diff "$base/../python/requirements.txt"
+
