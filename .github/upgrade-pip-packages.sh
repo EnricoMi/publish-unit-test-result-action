dd0a084d5a5ef4febee4486e5c8511056c34bffc
Enrico Minack <github@enrico.minack.dev>
2023-03-14 11:07:40 +0100

    Quote packages

--- .github/upgrade-pip-packages.sh
+++ .github/upgrade-pip-packages.sh
@@ -7,7 +7,7 @@ pip install --upgrade --upgrade-strategy eager -r "$base/../python/requirements-
 
 pip install pipdeptree
 grep -e "^#" -e ";" "$base/../python/requirements-direct.txt" > "$base/../python/requirements.txt"
-pipdeptree --packages=$(grep -v "^#" "$base/../python/requirements-direct.txt" | sed -e "s/;.*//" -e "s/=.*//g" | paste -s -d ,) --freeze >> "$base/../python/requirements.txt"
+pipdeptree --packages="$(grep -v "^#" "$base/../python/requirements-direct.txt" | sed -e "s/;.*//" -e "s/=.*//g" | paste -s -d ,)" --freeze >> "$base/../python/requirements.txt"
 
 git diff "$base/../python/requirements.txt"
 
