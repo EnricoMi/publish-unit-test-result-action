cd3b73a5ad30668afc07cc8effb4371f9a31d805
Enrico Minack <github@enrico.minack.dev>
2023-07-11 21:50:00 +0200

    Remove PyJWT constraint

--- .github/upgrade-pip-packages.sh
+++ .github/upgrade-pip-packages.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 base="$(dirname "$0")"
 
 pip install --upgrade --force pip==22.0.0
-pip install --upgrade --upgrade-strategy eager -r "$base/../python/requirements-direct.txt" -c "$base/../python/constraints.txt"
+pip install --upgrade --upgrade-strategy eager -r "$base/../python/requirements-direct.txt"
 
 pip install pipdeptree
 pipdeptree --packages="$(sed -e "s/;.*//" -e "s/=.*//g" "$base/../python/requirements-direct.txt" | paste -s -d ,)" --freeze > "$base/../python/requirements.txt"
